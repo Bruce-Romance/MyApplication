03d0e9ce9b95b69532661e80940177af95970361
BR <906563872@qq.com>
2018-06-15 17:46:41 +0800

    完善权限用户点击不在询问判断

--- app/src/main/java/yomix/yt/com/myapplication/PermissionActivity.java
+++ app/src/main/java/yomix/yt/com/myapplication/PermissionActivity.java
@@ -1,14 +1,22 @@
 package yomix.yt.com.myapplication;
 
 import android.Manifest;
-import android.content.BroadcastReceiver;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import dialog.DialogUtils;
+import dialog.MessageDialog;
+import dialog.onMessageDialogClick;
 import permission.PermissionsUtils;
+import toast.ToastUtils;
 
 public class PermissionActivity extends AppCompatActivity {
 
@@ -41,7 +49,8 @@ public class PermissionActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //拍照
-                PermissionsUtils.with(PermissionActivity.this).addPermission(Manifest.permission.CAMERA).initPermission();
+//                    PermissionsUtils.with(PermissionActivity.this).addPermission(Manifest.permission.CAMERA).initPermission();
+                ActivityCompat.requestPermissions(PermissionActivity.this, new String[]{Manifest.permission.CAMERA}, 1);
             }
         });
     }
@@ -49,6 +58,30 @@ public class PermissionActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for (int i = 0; i < grantResults.length; i++) {
 
+            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[i])) {
+                    //表明用户没有彻底禁止弹出权限请求
+                    ToastUtils.error("拒绝权限");
+                } else {
+                    new DialogUtils().messageDialog(PermissionActivity.this, "申请权限", "是否去设置中开启权限?", true, new onMessageDialogClick() {
+                        @Override
+                        public void confirm(MessageDialog dialog) {
+                            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                            intent.setData(Uri.parse("package:" + getPackageName()));
+                            startActivity(intent);
+                        }
+
+                        @Override
+                        public void cancel(MessageDialog dialog) {
+
+                        }
+                    });
+                }
+            } else {
+                ToastUtils.success("允许权限");
+            }
+        }
     }
 }
